0e19ab6e50950819f023a9364a6d3aec38fcc0b2
zhugelongfei <18519720472@163.com>
2017-05-06 18:48:34 +0800

    Support search filed

--- AutoExportUIScriptEditor/Editor/OutLineWindow/OutLineWindow.cs
+++ AutoExportUIScriptEditor/Editor/OutLineWindow/OutLineWindow.cs
@@ -11,14 +11,24 @@ namespace AutoExportScriptData
         }
 
         public static string windowName = "Data Out Line";
+        private Vector2 viewPos;
+        private string searchTxt = "";
 
         public new void OnGUI()
         {
+            GUILayout.Label("Search:");
+            searchTxt = GUILayout.TextField(searchTxt, GUILayout.Width(200));
+            DrawParagraphSplitSymbol();
+
             if (base.OnGUI())
                 return;
 
             //Format
             Dictionary<string, List<UIProgramData>> className_Data_Dic = FormatUIProgramDataArrayToDic(pDataArray);
+
+            bool isSearchTarget = false;
+
+            viewPos = GUILayout.BeginScrollView(viewPos);
 
             foreach (var item in className_Data_Dic)
             {
@@ -27,9 +37,31 @@ namespace AutoExportScriptData
                 GUILayout.Label("ClassName:" + item.Key);
                 foreach (UIProgramData curObj in item.Value)
                 {
-                    DrawUIProgramData(curObj);
+                    if (string.IsNullOrEmpty(searchTxt))
+                    {
+                        //Not find
+                        DrawUIProgramData(curObj);
+                    }
+                    else
+                    {
+                        //Find search target
+                        isSearchTarget = false;
+
+                        foreach (var exportData in curObj.ExportData)
+                        {
+                            if (exportData.VariableName.Contains(searchTxt))
+                            {
+                                isSearchTarget = true;
+                                break;
+                            }
+                        }
+                        if (isSearchTarget)
+                            DrawUIProgramData(curObj);
+                    }
                 }
             }
+
+            GUILayout.EndScrollView();
         }
 
         private Dictionary<string, List<UIProgramData>> FormatUIProgramDataArrayToDic(UIProgramData[] pDataArray)
@@ -45,12 +77,9 @@ namespace AutoExportScriptData
                     AddValue(className_Data_Dic, curObj.CreateClassName, curObj);
                 }
 
-                foreach (UIExportData data in curObj.ExportData)
-                {
-                    string className = string.IsNullOrEmpty(curObj.LocalClassName) ? this.data.clsName : curObj.LocalClassName;
+                string className = string.IsNullOrEmpty(curObj.LocalClassName) ? this.data.clsName : curObj.LocalClassName;
 
-                    AddValue(className_Data_Dic, className, curObj);
-                }
+                AddValue(className_Data_Dic, className, curObj);
             }
             return className_Data_Dic;
         }
